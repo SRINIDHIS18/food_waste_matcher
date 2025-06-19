f4c4f22a957855aa71dcec3a237ea1f74c16f536
SRINIDHIS18 <141949093+SRINIDHIS18@users.noreply.github.com>
2025-06-19 14:58:42 +0530

    Delete pages directory

--- pages/logout.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def logout_page():
-    st.session_state.auth = False
-    st.session_state.email = ""
-    st.session_state.role = None
-    st.success("🔓 You have been logged out.")
